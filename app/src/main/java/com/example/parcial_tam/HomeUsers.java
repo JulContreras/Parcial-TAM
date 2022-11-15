67a2839b116003c965aa28500b0e0a03765940a8
dagonc <dagonzalezc@uniboyaca.edu.co>
2022-11-15 15:54:57 -0500

    botones

--- app/src/main/java/com/example/parcial_tam/HomeUsers.java
+++ app/src/main/java/com/example/parcial_tam/HomeUsers.java
@@ -2,7 +2,10 @@ package com.example.parcial_tam;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class HomeUsers extends AppCompatActivity {
 
@@ -10,5 +13,15 @@ public class HomeUsers extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home_users);
+        Button Exit;
+        Exit = findViewById(R.id.BtnSalir);
+        Exit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, Register.class);
+                startActivity(intent);
+            }
+        });
+    }
     }
 }
