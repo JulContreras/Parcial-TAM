e18853049fdc3cbbe5cb81a07755495bf8b0b03d
dagonc <dagonzalezc@uniboyaca.edu.co>
2022-11-14 22:14:30 -0500

    Layouts completados

---
+++ app/src/main/java/com/example/parcial_tam/UsersAdmin.java
@@ -0,0 +1,14 @@
+package com.example.parcial_tam;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class UsersAdmin extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_users_admin);
+    }
+}
